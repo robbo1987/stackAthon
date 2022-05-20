5781ee3b41a8dbb0d41d0537f8baeddea711d8f6
Robert Albenda <ralbenda@gmail.com>
2022-05-19 20:13:23 -0400

    progress

--- src/components/Tasks/Modals/AddTask.vue
+++ src/components/Tasks/Modals/AddTask.vue
@@ -6,80 +6,90 @@
       <q-btn flat round dense v-close-popup icon="close" />
     </q-card-section>
 
-    <q-card-section class="q-pt-none">
-
+    <form @submit.prevent="submitForm">
+      <q-card-section class="q-pt-none">
         <div class="rov q-mb-sm">
-       <q-input
-        outlined
-        v-model="taskToSubmit.name"
-        label="Task name"
-        class="col"
-        />
+          <q-input
+            outlined
+            v-model="taskToSubmit.name"
+            :rules="[(val) => !!val || 'Field is required']"
+            ref="name"
+            label="Task name"
+            class="col"
+          />
         </div>
 
         <div class="rov q-mb-sm">
-        <q-input 
-        outlined
-        v-model="taskToSubmit.dueDate" 
-        label= "Due Date"
-        >
-         <template v-slot:append>
-            <q-icon name="event" class="cursor-pointer">
-              <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                <q-date v-model="taskToSubmit.dueDate">
-                  <div class="row items-center justify-end">
-                    <q-btn v-close-popup label="Close" color="primary" flat />
-                  </div>
-                </q-date>
-              </q-popup-proxy>
-            </q-icon>
-          </template>
-          
-        </q-input>
+          <q-input outlined v-model="taskToSubmit.dueDate" label="Due Date">
+            <template v-slot:append>
+              <q-icon name="event" class="cursor-pointer">
+                <q-popup-proxy
+                  cover
+                  transition-show="scale"
+                  transition-hide="scale"
+                >
+                  <q-date v-model="taskToSubmit.dueDate">
+                    <div class="row items-center justify-end">
+                      <q-btn v-close-popup label="Close" color="primary" flat />
+                    </div>
+                  </q-date>
+                </q-popup-proxy>
+              </q-icon>
+            </template>
+          </q-input>
         </div>
 
         <div class="rov q-mb-sm">
-        <q-input 
-        outlined
-        label="Due Time"
-        v-model="taskToSubmit.dueTime" >
-
-         <template v-slot:append>
-            <q-icon name="access_time" class="cursor-pointer">
-              <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                <q-time v-model="taskToSubmit.dueTime">
-                  <div class="row items-center justify-end">
-                    <q-btn v-close-popup label="Close" color="primary" flat />
-                  </div>
-               </q-time>
-             </q-popup-proxy>
-            </q-icon>
-          </template>
-        </q-input>
+          <q-input outlined label="Due Time" v-model="taskToSubmit.dueTime">
+            <template v-slot:append>
+              <q-icon name="access_time" class="cursor-pointer">
+                <q-popup-proxy
+                  cover
+                  transition-show="scale"
+                  transition-hide="scale"
+                >
+                  <q-time v-model="taskToSubmit.dueTime">
+                    <div class="row items-center justify-end">
+                      <q-btn v-close-popup label="Close" color="primary" flat />
+                    </div>
+                  </q-time>
+                </q-popup-proxy>
+              </q-icon>
+            </template>
+          </q-input>
         </div>
-     
-    </q-card-section>
-   
+      </q-card-section>
 
-    <q-card-actions align="right">
-      <q-btn flat label="Save" color="primary" v-close-popup />
-    </q-card-actions>
+      <q-card-actions align="right">
+        <q-btn flat label="Save" color="primary" type="submit" />
+      </q-card-actions>
+    </form>
   </q-card>
 </template>
 
 <script>
 export default {
-
   data() {
-    return{
+    return {
       taskToSubmit: {
-        name: '',
-        dueDate: '',
-        dueTime: '',
-        completed: false
+        name: "",
+        dueDate: "",
+        dueTime: "",
+        completed: false,
+      },
+    };
+  },
 
+  methods: {
+    submitForm() {
+      this.$refs.name.validate();
+      if (!this.$refs.name.hasError) {
+        this.submitTask();
       }
-    }
-  }
+    },
+    submitTask() {
+      console.log("submit task");
+    },
+  },
 };
 </script>
